c281ee09e7ba5cbbd800f2e76b3326006974a3c8
Ramiro <ramiroariel_carp@hotmail.com>
2026-06-24 22:04:03 -0300

    fix: center footer seller action

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -24,7 +24,7 @@ import { formatMoney } from "../../lib/helpers";
 import { Donation, MercadoPagoButton } from "../shared/Donation";
 import { Brand } from "./Brand";
 
-const APP_VERSION = "v0.53.0";
+const APP_VERSION = "v0.54.0";
 
 export function AppLayout({
   children,
